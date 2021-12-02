850fce6d598db280b995c0f0a2c807d3ead9e37f
SAURABH SINGH <58776330+saurabhgssingh@users.noreply.github.com>
2021-12-02 16:23:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+
+from flask import Flask, send_file
+app =Flask(__name__)
+
+@app.route('/')
+def hello():
+    print('Hello World')
+
+@app.route('/download')
+def download():
+    file_path = "report.pdf"
+    return send_file(file_path,as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
